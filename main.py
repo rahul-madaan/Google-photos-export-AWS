af771652048e96a2d371fcf067b967d52c95d41a
Rahul Madan <madan.rahul9@gmail.com>
2023-02-20 00:06:20 +0530

    printed mis-matching json files and unmapped media files, updated Todos

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import glob
+from pathlib import Path
 
 folder_path = '/Users/rahul.madan/Downloads/Takeout/Google Photos/Nov 2021/'
-
-Total = glob.glob(folder_path+'*')
+total = glob.glob(folder_path+'*')
 json = glob.glob(folder_path+'*.json')
 mp3 = glob.glob(folder_path+'*.mp4')
 jpg = glob.glob(folder_path+'*.jpg')
@@ -24,12 +24,12 @@ all_media.extend([image[len(folder_path):] for image in mov])
 
 all_metadata.extend([metadata[len(folder_path):] for metadata in json])
 
-all_files.extend([files[len(folder_path):] for files in Total])
+all_files.extend([files[len(folder_path):] for files in total])
 
 print(all_files)
 print("Total files: ", len(all_files))
 print("Total metadata files: ", len(all_metadata))
-print("Total media files: ", len(all_metadata))
+print("Total media files: ", len(all_media))
 
 missing = []
 
@@ -45,5 +45,22 @@ for media in all_media:
     if media + ".json" not in all_metadata:
         missing.append(media)
 
-print("\nItems which do not have metadata file or metadata file is not named correctly: \n", missing)
+print()
+print(len(missing))
+print("media which do not have metadata file or metadata file is not named correctly: \n", missing)
 
+missing = []
+for metadata in all_metadata:
+    if Path(metadata).stem not in all_media:
+        missing.append(metadata)
+
+print(len(missing))
+print("Json files which are not used: \n", missing)
+
+# write a code to detect image names which have name longer than 46 characters (excluding extension .mp3, .jpeg) and rename their json files
+
+# write a code to make duplicate .json file if 'edited' present in media file name
+
+# write a code to rename json files which have '(1)' in .json file name
+
+print(len('Screenshot_2021-11-04-17-28-16-732_com.whatsap'))
